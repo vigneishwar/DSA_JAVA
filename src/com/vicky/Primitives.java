7f26fdfd5f22e60902fe06be35b257b2e7bd462f
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-12-06 22:42:03 +1100

    primitive dtypes

---
+++ src/com/vicky/Primitives.java
@@ -0,0 +1,12 @@
+package com.vicky;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 34;
+        char letter = 'v';
+        float marks = 4.45f;
+        double decNum = 454453554.5433223;
+        long longNum = 43432432432432L;
+        boolean checkBool = true;
+    }
+}
