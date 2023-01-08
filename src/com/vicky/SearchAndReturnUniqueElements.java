76c8d767342581b903500438252636d23f5319a8
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2023-01-08 15:43:15 +1100

    refactored

--- src/com/vicky/SearchAndReturnUniqueElements.java
+++ src/com/vicky/SearchAndReturnUniqueElements.java
@@ -1,38 +1,37 @@
 package com.vicky;
 
 public class SearchAndReturnUniqueElements {
-
     public static int singleUniqueElement(int[] nums) {
         int left = 0;
-        int right = nums.length -1;
+        int right = nums.length - 1;
 
-        while(left <= right) {
+        //checking base cases when the size of the array is small
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[right] != nums[right - 1]) {
+            return nums[right];
+        }
+
+        while (left < right) {
             int middle = left + (right - left) / 2;
-            if (middle == 0) {
-                if(nums[middle] != nums[middle + 1]) {
-                    return nums[middle];
-                } else {
-                    left = middle + 1;
-                }
+            if (middle % 2 == 1) {  // checking if the middle is odd. If odd decrement it and make it even
+                middle--;
             }
-            else if (middle == nums.length-1) {
-                if(nums[middle] != nums[middle - 1]){
-                    return nums[middle];
-                } else {
-                    right = middle - 1;
-                }
+
+            if (nums[middle] == nums[middle + 1]) {
+                left = middle + 2;
             } else {
-                if (nums[middle] != nums[middle - 1] && nums[middle] != nums[middle + 1]) {
-                    return nums[middle];
-                } else if (nums[middle] == nums[middle - 1]) {
-                    left = middle + 1;
-                }else {
-                    right = middle - 1;
-                }
+                right = middle;
             }
         }
-        return -1;
+
+        return nums[left];
     }
+
 
     public static void main(String[] args) {
         int[]nums = {1,1,2,3,3,4,4,8,8};
